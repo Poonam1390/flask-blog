10aa6bd650976a4214d92fb95d27424effca0823
poonam1390 <umpoonam@yahoo.co.in>
2020-03-10 12:43:19 -0400

    forms and post page in flask blog - 10th March

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,14 @@
 # import Flask class from the flask module
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 # create a new instance of Flask and store it in app 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+
 # import the ./application/routes.py file
+db=SQLAlchemy(app)
+
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,25 +1,38 @@
-# import render_template function from the flask module
-from flask import render_template
-# import the app object from the ./application/__init__.py
-from application import app
-# define routes for / & /home, this function will be called when these are accessed
-blogData = [
-    {  
-        "name": {"first":"John", "last":"Doe"},
-        "title":"First Post",
-        "content":"This is some blog data for Flask lectures"
-    },
-    {   
-        "name": {"first":"Jane", "last":"Doe"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
+from application.models import Posts
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.forms import PostForm
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
+
+
 @app.route('/')
 @app.route('/home')
-
 def home():
- return render_template('home.html', title='HomePage',blogData=blogData)
+    postData = Posts.query.all()
+    #postfirstname = Posts.query.get(first_name)
+    #postlastname = Posts.query.get(last_name)
+    #postcontent = Posts.query.get(content)
+    return render_template('home.html', title='HomePage',posts=postData)
 
 
 @app.route('/about')
