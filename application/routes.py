4b76b59375ff09a3dbfb0ff37c7ee997006a3eec
poonam1390 <umpoonam@yahoo.co.in>
2020-03-12 11:35:16 -0400

    CRUD ready blog_post app code

--- application/routes.py
+++ application/routes.py
@@ -2,15 +2,17 @@ from flask_login import login_user, current_user, logout_user, login_required
 from application.models import Posts, Users
 from flask import render_template, redirect, url_for, request
 from application import app, db, bcrypt
-from application.forms import PostForm, RegistrationForm, LoginForm
+from application.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hash_pw = bcrypt.generate_password_hash(form.password.data)
 
-        user = Users(email=form.email.data, password=hash_pw)
+        user = Users(first_name=form.first_name.data,last_name=form.last_name.data,email=form.email.data, password=hash_pw)
 
         db.session.add(user)
         db.session.commit()
@@ -25,8 +27,7 @@ def post():
     form = PostForm()
     if form.validate_on_submit():
         postData = Posts(
-            first_name = form.first_name.data,
-            last_name = form.last_name.data,
+            author = current_user,
             title = form.title.data,
             content = form.content.data
         )
@@ -78,3 +79,35 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name        
+        form.email.data = current_user.email        
+    return render_template('account.html', title='Account', form=form)
+
+
+@app.route("/account/delete", methods=["GET", "POST"])
+@login_required
+def account_delete():
+    user = current_user.id
+    account = Users.query.filter_by(id=user).first()
+    logout_user()
+    posts = Posts.query.filter_by(user_id=user)
+    for post in posts:
+        db.session.delete(post)
+
+    db.session.delete(account)
+    db.session.commit()
+
+    return redirect(url_for('register'))
